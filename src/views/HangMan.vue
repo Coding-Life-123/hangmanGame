2a4f0f386b2f5cf1216bbcc2158be7a39813dd27
Coding-Life-123 <dfordonezpina@gmail.com>
2025-11-27 20:45:23 -0500

    hola

--- src/views/HangMan.vue
+++ src/views/HangMan.vue
@@ -30,17 +30,21 @@ function browseChar(letter){
     }
 }
 
-browseChar("b")
 
 console.log(chars.value, display.value)
+console.log(wrongLetters.value);
 
 </script>
 
 <template>
     <div class="hangman-main">
         <div class="hangman-content">
+            <img class="hangman-image" :src="'./hangman'+wrongLetters.length+'.png'" alt="">
             <div class="hangman-game">
                 <h1 v-for="(char, index) in display" :key="index">{{ char }}</h1>
+            </div>
+            <div class="hangman-keyboard">
+                <button v-for="(letter, i) in letters" :key="i" @click="browseChar(letter)">{{ letter }}</button>
             </div>
         </div>
     </div>
@@ -61,14 +65,60 @@ console.log(chars.value, display.value)
         border-radius: 15px;
         box-shadow: 0 0 5px 5px white;
         margin: auto;
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+
+    .hangman-image{
+        width: 300px;
+        margin: auto auto 0 auto;
     }
 
     .hangman-game{
         display: flex;
+        margin: 0 auto;
+        height: fit-content;    
+        width: fit-content;
     }
 
     .hangman-game h1{
-        margin: 0 5px;
+        margin: 0 10px;
+    }
 
+    .hangman-keyboard{
+        margin: auto;
+        margin-top: 0;
+        width: 60%;
+        justify-content: center;
+        text-align: center;
+    }
+
+    .hangman-keyboard button{
+        padding: 10px;
+        font-size: 18px;
+        position: relative;
+        border-radius: 10px;
+        background-color: gray;
+        margin: 5px;
+        border: none;
+        width: 40px;
+        cursor: pointer;
+        z-index: 1;
+        overflow: hidden;
+    }
+
+    .hangman-keyboard button::before{
+        content: "";
+        position: absolute;
+        background-color: rgb(32, 32, 32);
+        inset: 0;
+        opacity: 0;
+        transition: opacity 0.2s ease;
+        z-index: -1;
+    }
+
+    .hangman-keyboard button:hover::before{
+        opacity: 0.3;
     }
 </style>
